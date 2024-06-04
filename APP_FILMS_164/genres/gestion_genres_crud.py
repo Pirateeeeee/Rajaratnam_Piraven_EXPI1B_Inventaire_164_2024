ecb58d846c15fabccaa95ff11cce1aaf6015bfd6
Rajaratnam_Piraven_Expi1b_inventaire_2024 <Piraven.Rajaratnam@epfl.ch>
2024-06-04 21:12:39 +0200

    ok

--- APP_FILMS_164/genres/gestion_genres_crud.py
+++ APP_FILMS_164/genres/gestion_genres_crud.py
@@ -4,27 +4,22 @@ Auteur : OM 2021.03.16
 """
 from pathlib import Path
 
-from flask import redirect
-from flask import request
-from flask import session
-from flask import url_for
-
+from flask import redirect, request, session, url_for, render_template, flash
 from APP_FILMS_164 import app
 from APP_FILMS_164.database.database_tools import DBconnection
 from APP_FILMS_164.erreurs.exceptions import *
-from APP_FILMS_164.genres.gestion_genres_wtf_forms import FormWTFAjouterGenres
-from APP_FILMS_164.genres.gestion_genres_wtf_forms import FormWTFDeleteComputer
-from APP_FILMS_164.genres.gestion_genres_wtf_forms import FormWTFUpdateGenre
+from APP_FILMS_164.genres.gestion_genres_wtf_forms import FormWTFAjouterGenres, FormWTFDeleteComputer, \
+    FormWTFUpdateGenre
 
 """
     Auteur : OM 2021.03.16
-    Définition d'une "route" /genres_afficher
-    
+    Définition d'une "route" /computers_afficher
+
     Test : ex : http://127.0.0.1:5575/computers_afficher
-    
+
     Paramètres : order_by : ASC : Ascendant, DESC : Descendant
-                id_genre_sel = 0 >> tous les genres.
-                id_genre_sel = "n" affiche le genre dont l'id est "n"
+                id_computer_sel = 0 >> tous les ordinateurs.
+                id_computer_sel = "n" affiche l'ordinateur dont l'id est "n"
 """
 
 
@@ -37,62 +32,41 @@ def computers_afficher(order_by, id_computer_sel):
                     strsql_computers_afficher = """SELECT * FROM t_computer ORDER BY idComputer ASC"""
                     mc_afficher.execute(strsql_computers_afficher)
                 elif order_by == "ASC":
-                    # C'EST LA QUE VOUS ALLEZ DEVOIR PLACER VOTRE PROPRE LOGIQUE MySql
-                    # la commande MySql classique est "SELECT * FROM t_genre"
-                    # Pour "lever"(raise) une erreur s'il y a des erreurs sur les noms d'attributs dans la table
-                    # donc, je précise les champs à afficher
-                    # Constitution d'un dictionnaire pour associer l'id du genre sélectionné avec un nom de variable
-
                     valeur_id_computer_selected_dictionnaire = {"value_id_computer_selected": id_computer_sel}
                     strsql_computers_afficher = """SELECT * FROM t_computer WHERE idComputer = %(value_id_computer_selected)s"""
-
                     mc_afficher.execute(strsql_computers_afficher, valeur_id_computer_selected_dictionnaire)
                 else:
-                    strsql_computers_afficher = """SELECT *  FROM t_computer ORDER BY idComputer DESC"""
-
+                    strsql_computers_afficher = """SELECT * FROM t_computer ORDER BY idComputer DESC"""
                     mc_afficher.execute(strsql_computers_afficher)
 
                 data_computers = mc_afficher.fetchall()
 
                 print("data_computers ", data_computers, " Type : ", type(data_computers))
 
-                # Différencier les messages si la table est vide.
                 if not data_computers and id_computer_sel == 0:
-                    flash("""La table "t_genre" est vide. !!""", "warning")
+                    flash("""La table "t_computer" est vide. !!""", "warning")
                 elif not data_computers and id_computer_sel > 0:
-                    # Si l'utilisateur change l'id_genre dans l'URL et que le genre n'existe pas,
-                    flash(f"Aucun ordinateur n'a été trouvé !!", "warning")
+                    flash(f"Aucun ordinateur trouvé !!", "warning")
                 else:
-                    # Dans tous les autres cas, c'est que la table "t_genre" est vide.
-                    # OM 2020.04.09 La ligne ci-dessous permet de donner un sentiment rassurant aux utilisateurs.
-                    flash(f"Données des ordinateurs affichés !!", "success")
+                    flash(f"Données des ordinateurs affichées !!", "success")
 
         except Exception as Exception_computers_afficher:
             raise ExceptionComputersAfficher(f"fichier : {Path(__file__).name}  ;  "
-                                          f"{computers_afficher.__name__} ; "
-                                          f"{Exception_computers_afficher}")
+                                             f"{computers_afficher.__name__} ; "
+                                             f"{Exception_computers_afficher}")
 
-    # Envoie la page "HTML" au serveur.
     return render_template("computers/computers_afficher.html", data=data_computers)
 
 
 """
     Auteur : OM 2021.03.22
     Définition d'une "route" /genres_ajouter
-    
+
     Test : ex : http://127.0.0.1:5575/genres_ajouter
-    
+
     Paramètres : sans
-    
+
     But : Ajouter un genre pour un film
-    
-    Remarque :  Dans le champ "name_genre_html" du formulaire "genres/genres_ajouter.html",
-                le contrôle de la saisie s'effectue ici en Python.
-                On transforme la saisie en minuscules.
-                On ne doit pas accepter des valeurs vides, des valeurs avec des chiffres,
-                des valeurs avec des caractères qui ne sont pas des lettres.
-                Pour comprendre [A-Za-zÀ-ÖØ-öø-ÿ] il faut se reporter à la table ASCII https://www.ascii-code.com/
-                Accepte le trait d'union ou l'apostrophe, et l'espace entre deux mots, mais pas plus d'une occurence.
 """
 
 
@@ -114,7 +88,6 @@ def genres_ajouter_wtf():
                 flash(f"Données insérées !!", "success")
                 print(f"Données insérées !!")
 
-                # Pour afficher et constater l'insertion de la valeur, on affiche en ordre inverse. (DESC)
                 return redirect(url_for('genres_afficher', order_by='DESC', id_genre_sel=0))
 
         except Exception as Exception_genres_ajouter_wtf:
@@ -128,70 +101,47 @@ def genres_ajouter_wtf():
 """
     Auteur : OM 2021.03.29
     Définition d'une "route" /genre_update
-    
+
     Test : ex cliquer sur le menu "genres" puis cliquer sur le bouton "EDIT" d'un "genre"
-    
+
     Paramètres : sans
-    
+
     But : Editer(update) un genre qui a été sélectionné dans le formulaire "genres_afficher.html"
-    
-    Remarque :  Dans le champ "nom_genre_update_wtf" du formulaire "genres/genre_update_wtf.html",
-                le contrôle de la saisie s'effectue ici en Python.
-                On transforme la saisie en minuscules.
-                On ne doit pas accepter des valeurs vides, des valeurs avec des chiffres,
-                des valeurs avec des caractères qui ne sont pas des lettres.
-                Pour comprendre [A-Za-zÀ-ÖØ-öø-ÿ] il faut se reporter à la table ASCII https://www.ascii-code.com/
-                Accepte le trait d'union ou l'apostrophe, et l'espace entre deux mots, mais pas plus d'une occurence.
 """
 
 
 @app.route("/genre_update", methods=['GET', 'POST'])
 def genre_update_wtf():
-    # L'utilisateur vient de cliquer sur le bouton "EDIT". Récupère la valeur de "id_genre"
     id_genre_update = request.values['id_genre_btn_edit_html']
-
-    # Objet formulaire pour l'UPDATE
     form_update = FormWTFUpdateGenre()
     try:
-        # 2023.05.14 OM S'il y a des listes déroulantes dans le formulaire
-        # La validation pose quelques problèmes
         if request.method == "POST" and form_update.submit.data:
-            # Récupèrer la valeur du champ depuis "genre_update_wtf.html" après avoir cliqué sur "SUBMIT".
-            # Puis la convertir en lettres minuscules.
-            name_genre_update = form_update.nom_genre_update_wtf.data
-            name_genre_update = name_genre_update.lower()
+            name_genre_update = form_update.nom_genre_update_wtf.data.lower()
             date_genre_essai = form_update.date_genre_wtf_essai.data
 
             valeur_update_dictionnaire = {"value_id_genre": id_genre_update,
                                           "value_name_genre": name_genre_update,
-                                          "value_date_genre_essai": date_genre_essai
-                                          }
+                                          "value_date_genre_essai": date_genre_essai}
             print("valeur_update_dictionnaire ", valeur_update_dictionnaire)
 
             str_sql_update_intitulegenre = """UPDATE t_genre SET intitule_genre = %(value_name_genre)s, 
-            date_ins_genre = %(value_date_genre_essai)s WHERE id_genre = %(value_id_genre)s """
+                                              date_ins_genre = %(value_date_genre_essai)s WHERE id_genre = %(value_id_genre)s"""
             with DBconnection() as mconn_bd:
                 mconn_bd.execute(str_sql_update_intitulegenre, valeur_update_dictionnaire)
 
             flash(f"Donnée mise à jour !!", "success")
             print(f"Donnée mise à jour !!")
 
-            # afficher et constater que la donnée est mise à jour.
-            # Affiche seulement la valeur modifiée, "ASC" et l'"id_genre_update"
             return redirect(url_for('genres_afficher', order_by="ASC", id_genre_sel=id_genre_update))
         elif request.method == "GET":
-            # Opération sur la BD pour récupérer "id_genre" et "intitule_genre" de la "t_genre"
-            str_sql_id_genre = "SELECT id_genre, intitule_genre, date_ins_genre FROM t_genre " \
-                               "WHERE id_genre = %(value_id_genre)s"
+            str_sql_id_genre = "SELECT id_genre, intitule_genre, date_ins_genre FROM t_genre WHERE id_genre = %(value_id_genre)s"
             valeur_select_dictionnaire = {"value_id_genre": id_genre_update}
             with DBconnection() as mybd_conn:
                 mybd_conn.execute(str_sql_id_genre, valeur_select_dictionnaire)
-            # Une seule valeur est suffisante "fetchone()", vu qu'il n'y a qu'un seul champ "nom genre" pour l'UPDATE
             data_nom_genre = mybd_conn.fetchone()
             print("data_nom_genre ", data_nom_genre, " type ", type(data_nom_genre), " genre ",
                   data_nom_genre["intitule_genre"])
 
-            # Afficher la valeur sélectionnée dans les champs du formulaire "genre_update_wtf.html"
             form_update.nom_genre_update_wtf.data = data_nom_genre["intitule_genre"]
             form_update.date_genre_wtf_essai.data = data_nom_genre["date_ins_genre"]
 
@@ -205,27 +155,21 @@ def genre_update_wtf():
 
 """
     Auteur : OM 2021.04.08
-    Définition d'une "route" /genre_delete
-    
+    Définition d'une "route" /computer_delete
+
     Test : ex. cliquer sur le menu "genres" puis cliquer sur le bouton "DELETE" d'un "genre"
-    
+
     Paramètres : sans
-    
+
     But : Effacer(delete) un genre qui a été sélectionné dans le formulaire "genres_afficher.html"
-    
-    Remarque :  Dans le champ "nom_genre_delete_wtf" du formulaire "genres/computer_delete_wtf.html",
-                le contrôle de la saisie est désactivée. On doit simplement cliquer sur "DELETE"
 """
 
 
 @app.route("/computer_delete", methods=['GET', 'POST'])
 def computers_delete_wtf():
-    data_films_attribue_genre_delete = None
+    data_notes_attribue_computer_delete = None
     btn_submit_del = None
-    # L'utilisateur vient de cliquer sur le bouton "DELETE". Récupère la valeur de "id_genre"
     id_computer_delete = request.values['id_computer_btn_delete_html']
-
-    # Objet formulaire pour effacer le genre sélectionné.
     form_delete = FormWTFDeleteComputer()
     try:
         print(" on submit ", form_delete.validate_on_submit())
@@ -235,77 +179,43 @@ def computers_delete_wtf():
                 return redirect(url_for("computers_afficher", order_by="ASC", id_computer_sel=0))
 
             if form_delete.submit_btn_conf_del.data:
-                # Récupère les données afin d'afficher à nouveau
-                # le formulaire "genres/computer_delete_wtf.html" lorsque le bouton "Etes-vous sur d'effacer ?" est cliqué.
-                data_films_attribue_genre_delete = session['data_films_attribue_genre_delete']
-                print("data_films_attribue_genre_delete ", data_films_attribue_genre_delete)
+                data_notes_attribue_computer_delete = session['data_notes_attribue_computer_delete']
+                print("data_notes_attribue_computer_delete ", data_notes_attribue_computer_delete)
 
-                flash(f"Effacer le genre de façon définitive de la BD !!!", "danger")
-                # L'utilisateur vient de cliquer sur le bouton de confirmation pour effacer...
-                # On affiche le bouton "Effacer genre" qui va irrémédiablement EFFACER le genre
+                flash(f"Effacer l'ordinateur de façon définitive de la BD !!!", "danger")
                 btn_submit_del = True
 
             if form_delete.submit_btn_del.data:
                 valeur_delete_dictionnaire = {"value_id_computer": id_computer_delete}
                 print("valeur_delete_dictionnaire ", valeur_delete_dictionnaire)
 
-                str_sql_delete_films_genre = """DELETE FROM t_genre_film WHERE fk_genre = %(value_id_computer)s"""
                 str_sql_delete_idcomputer = """DELETE FROM t_computer WHERE idComputer = %(value_id_computer)s"""
-                # Manière brutale d'effacer d'abord la "fk_genre", même si elle n'existe pas dans la "t_genre_film"
-                # Ensuite on peut effacer le genre vu qu'il n'est plus "lié" (INNODB) dans la "t_genre_film"
                 with DBconnection() as mconn_bd:
-                    mconn_bd.execute(str_sql_delete_films_genre, valeur_delete_dictionnaire)
                     mconn_bd.execute(str_sql_delete_idcomputer, valeur_delete_dictionnaire)
 
-                flash(f"Genre définitivement effacé !!", "success")
-                print(f"Genre définitivement effacé !!")
+                flash(f"Ordinateur définitivement effacé !!", "success")
+                print(f"Ordinateur définitivement effacé !!")
 
-                # afficher les données
                 return redirect(url_for('computers_afficher', order_by="ASC", id_computer_sel=0))
 
-        if request.method == "GET":
-            valeur_select_dictionnaire = {"value_id_computer": id_computer_delete}
-            print(id_computer_delete, type(id_computer_delete))
+    if request.method == "GET":
+        valeur_select_dictionnaire = {"value_id_computer": id_computer_delete}
+        print(id_computer_delete, type(id_computer_delete))
 
-            # Requête qui affiche tous les films_genres qui ont le genre que l'utilisateur veut effacer
-            str_sql_computer_note_delete = """SELECT * FROM t_computer_note 
-                                            INNER JOIN t_note ON t_computer_note.fk_note = t_note.idNote
-                                            INNER JOIN t_computer ON t_computer_note.fk_computer = t_computer.idComputer
-                                            WHERE fk_computer = %(value_id_computer)s"""
+        # Sélectionner l'ordinateur spécifique à supprimer
+        str_sql_id_computer = "SELECT * FROM t_computer WHERE idComputer = %(value_id_computer)s"
+        with DBconnection() as mydb_conn:
+            mydb_conn.execute(str_sql_id_computer, valeur_select_dictionnaire)
+            data_nom_computer = mydb_conn.fetchone()
+            print("data_nom_computer ", data_nom_computer, " type ", type(data_nom_computer))
 
-            with DBconnection() as mydb_conn:
-                mydb_conn.execute(str_sql_computer_note_delete, valeur_select_dictionnaire)
-                data_notes_attribue_computer_delete = mydb_conn.fetchall()
-                print("data_films_attribue_computer_delete...", data_notes_attribue_computer_delete)
+            # Vérifie si la clé 'computerName' existe dans le dictionnaire avant de l'utiliser
+            if data_nom_computer and 'computerName' in data_nom_computer:
+                form_delete.nom_computer_delete_wtf.data = data_nom_computer["computerName"]
+            else:
+                # Gérer le cas où la clé 'computerName' n'existe pas dans le dictionnaire
+                # ou si le dictionnaire est vide
+                form_delete.nom_computer_delete_wtf.data = "Nom de l'ordinateur non disponible"
 
-                # Nécessaire pour mémoriser les données afin d'afficher à nouveau
-                # le formulaire "genres/computer_delete_wtf.html" lorsque le bouton "Etes-vous sur d'effacer ?" est cliqué.
-                session['data_films_attribue_computer_delete'] = data_notes_attribue_computer_delete
-
-                # Opération sur la BD pour récupérer "id_genre" et "intitule_genre" de la "t_genre"
-                str_sql_id_computer = "SELECT idComputer, comHostname FROM t_computer WHERE idComputer = %(value_id_computer)s"
-
-                mydb_conn.execute(str_sql_id_computer, valeur_select_dictionnaire)
-                # Une seule valeur est suffisante "fetchone()",
-                # vu qu'il n'y a qu'un seul champ "nom genre" pour l'action DELETE
-                data_nom_computer = mydb_conn.fetchone()
-                print("data_nom_computer ", data_nom_computer, " type ", type(data_nom_computer), " computer ",
-                      data_nom_computer["comHostname"])
-
-            # Afficher la valeur sélectionnée dans le champ du formulaire "computer_delete_wtf.html"
-            form_delete.nom_computer_delete_wtf.data = data_nom_computer["comHostname"]
-
-            # Le bouton pour l'action "DELETE" dans le form. "computer_delete_wtf.html" est caché.
-            btn_submit_del = False
-
-    except Exception as Exception_genre_delete_wtf:
-        raise ExceptionComputerDeleteWtf(f"fichier : {Path(__file__).name}  ;  "
-                                      f"{computers_delete_wtf.__name__} ; "
-                                      f"{Exception_genre_delete_wtf}")
-
-    return render_template("computers/computer_delete_wtf.html",
-                           form_delete=form_delete,
-                           btn_submit_del=btn_submit_del,
-                           data_computer_associes=data_notes_attribue_computer_delete)
-
+        btn_submit_del = False
 
